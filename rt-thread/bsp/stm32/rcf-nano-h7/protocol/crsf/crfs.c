5ae0147d660c42b932df28c362580f2be012d084
Althrone <imitatores131718@icloud.com>
2024-12-06 13:50:07 +0800

    CRFS测试OK

--- rt-thread/bsp/stm32/rcf-nano-h7/protocol/crsf/crfs.c
+++ rt-thread/bsp/stm32/rcf-nano-h7/protocol/crsf/crfs.c
@@ -26,6 +26,22 @@
  *****************************************************************************/
 
 crsf_channels_s crsf_channels_value __attribute__((section(".test_data")));
+uint16_t ch0;
+uint16_t ch1;
+uint16_t ch2;
+uint16_t ch3;
+uint16_t ch4;
+uint16_t ch5;
+uint16_t ch6;
+uint16_t ch7;
+uint16_t ch8;
+uint16_t ch9;
+uint16_t ch10;
+uint16_t ch11;
+uint16_t ch12;
+uint16_t ch13;
+uint16_t ch14;
+uint16_t ch15;
 
 /******************************************************************************
  * private variables
@@ -52,8 +68,10 @@ void crfs_decode(rt_uint8_t* pbuf,rt_uint32_t size)
     rt_uint32_t crc;
 
     //计算CRC
+    // rt_uint8_t buf[]={0x29,0xEA,0xEE,0x53,0x49,0x59,0x49,0x20,0x46,0x4D,0x33,0x30,0x00,0x45,0x4C,0x52,0x53,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x13,0x00};
+    // crc=HAL_CRC_Calculate(&hcrc,buf,sizeof(buf));
     crc=HAL_CRC_Calculate(&hcrc,&pbuf[2],len-1);
-    if(crc!=pbuf[len])
+    if(crc!=pbuf[len+1])
         return;
 
     switch ((CrfsPacketTypesEnum)pbuf[2])
@@ -61,6 +79,22 @@ void crfs_decode(rt_uint8_t* pbuf,rt_uint32_t size)
     case CRSF_FRAMETYPE_RC_CHANNELS_PACKED:
 
         rt_memcpy(&crsf_channels_value,&pbuf[3],sizeof(crsf_channels_value));
+        ch0=crsf_channels_value.ch0;
+        ch1=crsf_channels_value.ch1;
+        ch2=crsf_channels_value.ch2;
+        ch3=crsf_channels_value.ch3;
+        ch4=crsf_channels_value.ch4;
+        ch5=crsf_channels_value.ch5;
+        ch6=crsf_channels_value.ch6;
+        ch7=crsf_channels_value.ch7;
+        ch8=crsf_channels_value.ch8;
+        ch9=crsf_channels_value.ch9;
+        ch10=crsf_channels_value.ch10;
+        ch11=crsf_channels_value.ch11;
+        ch12=crsf_channels_value.ch12;
+        ch13=crsf_channels_value.ch13;
+        ch14=crsf_channels_value.ch14;
+        ch15=crsf_channels_value.ch15;
         break;
     
     default:
