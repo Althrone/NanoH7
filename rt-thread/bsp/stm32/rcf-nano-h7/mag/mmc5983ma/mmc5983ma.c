ade1f447168d0d44e7dfb7bdc9e2699976b0bcdd
Althrone <imitatores131718@icloud.com>
2025-03-26 22:09:45 +0800

    磁力计修改为连续测量模式，数据正常了，但是和读温度的功能有干扰，看怎么弄成单次读取模式

--- rt-thread/bsp/stm32/rcf-nano-h7/mag/mmc5983ma/mmc5983ma.c
+++ rt-thread/bsp/stm32/rcf-nano-h7/mag/mmc5983ma/mmc5983ma.c
@@ -76,7 +76,8 @@ void mmc5893ma_init(rt_sensor_t sensor)
     //CR0
     gs_mmc5983ma_cr0.B.Auto_SR_en=1;//开启自动sr
     gs_mmc5983ma_cr0.B.INT_meas_done_en=1;//开启测量完成中断
-    // gs_mmc5983ma_cr0.B.TM_T=1,
+    gs_mmc5983ma_cr0.B.TM_M=1,//开启连续采样需要设置这一位x轴才有反应，不知道是不是假芯片
+    gs_mmc5983ma_cr0.B.TM_T=0,//开启连续采样需要设置这一位x轴才有反应，不知道是不是假芯片
 
     send_buf[0]=MMC5983MA_CR0_ADDR;
     send_buf[1]=gs_mmc5983ma_cr0.r;
@@ -85,51 +86,12 @@ void mmc5893ma_init(rt_sensor_t sensor)
     // cr2 CM_Freq
     gs_mmc5983ma_cr2.B.Cm_freq=7;
     gs_mmc5983ma_cr2.B.Cmm_en=1;
-    // .B.En_prd_set=1,
-    // Prd_set不知道干嘛用的
+    gs_mmc5983ma_cr2.B.En_prd_set=1;
+    gs_mmc5983ma_cr2.B.Prd_set=1;
 
     send_buf[0]=MMC5983MA_CR2_ADDR;
     send_buf[1]=gs_mmc5983ma_cr2.r;
     rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
-
-    //测试确实可以通过关闭连续采样->复位的方式彻底关闭连续采样
-    //但是关闭连续采样->清状态位的方法好像不行
-
-    // //读sr
-    // send_buf[0]=0x80|MMC5983MA_SR_ADDR;
-    // send_buf[1]=0;
-    // do
-    // {
-    //     rt_spi_transfer(spi_dev,send_buf,recv_buf,2);
-    // }while(((Mmc5893maStatRegUnion*)(&recv_buf[1]))->B.Meas_M_Done!=1);//等到数据ok
-
-    // send_buf[0]=MMC5983MA_CR2_ADDR;
-    // send_buf[1]=0;
-    // rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
-
-    // // mmc5893ma_reset(sensor);
-
-    // send_buf[0]=MMC5983MA_SR_ADDR;
-    // send_buf[1]=1;
-    // rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
-
-    // //读sr
-    // send_buf[0]=0x80|MMC5983MA_SR_ADDR;
-    // send_buf[1]=0;
-    // do
-    // {
-    //     rt_spi_transfer(spi_dev,send_buf,recv_buf,2);
-    // }while(((Mmc5893maStatRegUnion*)(&recv_buf[1]))->B.OTP_Rd_Done!=1);//等到数据ok
-
-    // //读sr
-    // send_buf[0]=0x80|MMC5983MA_SR_ADDR;
-    // send_buf[1]=0;
-    // do
-    // {
-    //     rt_spi_transfer(spi_dev,send_buf,recv_buf,2);
-    // }while(((Mmc5893maStatRegUnion*)(&recv_buf[1]))->B.Meas_M_Done!=1);//等到数据ok
-
-    // while(1);
 }
 
 void mmc5893ma_reset(rt_sensor_t sensor)
@@ -142,7 +104,7 @@ void mmc5893ma_reset(rt_sensor_t sensor)
     Mmc5893maCtrl1RegUnion cr1={
         .B.SW_RST=1,
     };
-    rt_uint8_t send_buf[]={MMC5983MA_CR1_ADDR,cr1.r};
+    rt_uint8_t send_buf[]={MMC5983MA_CR1_ADDR,0xff};
     rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
 
     rt_thread_mdelay(10);//MEMSIC MMC5983MA Rev A Page 15 of 20 Formal release date: 4/3/2019
@@ -301,25 +263,30 @@ rt_size_t _mmc5893ma_mag_polling_get_data(struct rt_sensor_device *sensor, struc
     if(rt_spi_transfer(spi_dev,send_buf,recv_buf,2)!=2)
         return 0;//传感器数据返回长度只有0和1，0表示失败
 
-
     send_buf[0]=0x80|MMC5983MA_X_OUT_0_ADDR;
     if(rt_spi_transfer(spi_dev,send_buf,recv_buf,8)!=8)
         return 0;//传感器数据返回长度只有0和1，0表示失败
 
-    rt_int32_t x=((recv_buf[1]&0x80)>0)?0xFFFC0000|(((rt_uint32_t)recv_buf[1])<<10)|(((rt_uint32_t)recv_buf[2])<<2)|((Mmc5893maXyzOut2RegUnion*)(&recv_buf[7]))->B.Xout:
-                                                   (((rt_uint32_t)recv_buf[1])<<10)|(((rt_uint32_t)recv_buf[2])<<2)|((Mmc5893maXyzOut2RegUnion*)(&recv_buf[7]))->B.Xout;
+    //获取到的值不是补码，而是一个无符号数
+    rt_int32_t x=(((rt_uint32_t)recv_buf[1])<<10)|(((rt_uint32_t)recv_buf[2])<<2)|((Mmc5893maXyzOut2RegUnion*)(&recv_buf[7]))->B.Xout;
+    rt_int32_t y=(((rt_uint32_t)recv_buf[3])<<10)|(((rt_uint32_t)recv_buf[4])<<2)|((Mmc5893maXyzOut2RegUnion*)(&recv_buf[7]))->B.Yout;
+    rt_int32_t z=(((rt_uint32_t)recv_buf[5])<<10)|(((rt_uint32_t)recv_buf[6])<<2)|((Mmc5893maXyzOut2RegUnion*)(&recv_buf[7]))->B.Zout;
 
-    rt_int32_t y=((recv_buf[3]&0x80)>0)?0xFFFC0000|(((rt_uint32_t)recv_buf[3])<<10)|(((rt_uint32_t)recv_buf[4])<<2)|((Mmc5893maXyzOut2RegUnion*)(&recv_buf[7]))->B.Yout:
-                                                   (((rt_uint32_t)recv_buf[3])<<10)|(((rt_uint32_t)recv_buf[4])<<2)|((Mmc5893maXyzOut2RegUnion*)(&recv_buf[7]))->B.Yout;
-
-    rt_int32_t z=((recv_buf[5]&0x80)>0)?0xFFFC0000|(((rt_uint32_t)recv_buf[5])<<10)|(((rt_uint32_t)recv_buf[6])<<2)|((Mmc5893maXyzOut2RegUnion*)(&recv_buf[7]))->B.Zout:
-                                                   (((rt_uint32_t)recv_buf[5])<<10)|(((rt_uint32_t)recv_buf[6])<<2)|((Mmc5893maXyzOut2RegUnion*)(&recv_buf[7]))->B.Zout;
-
+    x-=131072;
+    y-=131072;
+    z-=131072;
+    
     sensor_data->type = RT_SENSOR_CLASS_MAG;
     sensor_data->data.mag.x = x*0.0625;//系数是0.0625mG
     sensor_data->data.mag.y = y*0.0625;
     sensor_data->data.mag.z = z*0.0625;
     sensor_data->timestamp = rt_sensor_get_ts();
+
+    // gs_mmc5983ma_cr0.B.TM_M=1,//开启连续采样需要设置这一位x轴才有反应，不知道是不是假芯片
+    // gs_mmc5983ma_cr0.B.TM_T=0,//开启连续采样需要设置这一位x轴才有反应，不知道是不是假芯片
+    // send_buf[0]=MMC5983MA_CR0_ADDR;
+    // send_buf[1]=gs_mmc5983ma_cr0.r;
+    // rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
 
     return 1;
 }
@@ -335,24 +302,43 @@ rt_size_t _mmc5893ma_temp_polling_get_data(struct rt_sensor_device *sensor, stru
     rt_uint8_t send_buf[2]={0};
     rt_uint8_t recv_buf[2]={0};
 
-    //考虑增加清中断操作
-    //温度不做int清除的操作
+    //关闭连续采样
+    // send_buf[0]=MMC5983MA_CR2_ADDR;
+    // send_buf[1]=0;
+    // rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
+    // rt_thread_mdelay(20);
+
+    //直接复位拉倒
+    send_buf[0]=MMC5983MA_CR1_ADDR;
+    send_buf[1]=0xff;
+    rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
+    rt_thread_mdelay(10);//MEMSIC MMC5983MA Rev A Page 15 of 20 Formal release date: 4/3/2019
+
+    //读sr
+    send_buf[0]=0x80|MMC5983MA_SR_ADDR;
+    send_buf[1]=0;
+    do
+    {
+        rt_spi_transfer(spi_dev,send_buf,recv_buf,2);
+    }while(((Mmc5893maStatRegUnion*)(&recv_buf[1]))->B.OTP_Rd_Done!=1);
 
     //触发温度采样
-    // send_buf[0]=MMC5983MA_CR0_ADDR;
-    // send_buf[1]=gs_mmc5983ma_cr0.r|2;
-    // rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
+    // gs_mmc5983ma_cr0.B.TM_M=0;
+    // gs_mmc5983ma_cr0.B.TM_T=1;
+    send_buf[0]=MMC5983MA_CR0_ADDR;
+    send_buf[1]=2;
+    rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
 
     //读取状态
     do
     {
-        send_buf[0]=0x80|MMC5983MA_T_OUT_ADDR;
+        send_buf[0]=0x80|MMC5983MA_SR_ADDR;
         send_buf[1]=0;
         rt_spi_transfer(spi_dev,send_buf,recv_buf,2);
-        rt_thread_mdelay(1);
-    } while (1);
-    // } while ((recv_buf[1]&2)!=2);
+        // rt_thread_mdelay(1);
+    } while (((Mmc5893maStatRegUnion)recv_buf[1]).B.Meas_T_Done==0);
 
+    //读取数据
     send_buf[0]=0x80|MMC5983MA_T_OUT_ADDR;
     send_buf[1]=0x00;
     rt_spi_transfer(spi_dev,send_buf,recv_buf,2);
@@ -362,6 +348,31 @@ rt_size_t _mmc5893ma_temp_polling_get_data(struct rt_sensor_device *sensor, stru
     sensor_data->type = RT_SENSOR_CLASS_TEMP;
     sensor_data->data.temp=t*10;
     sensor_data->timestamp = rt_sensor_get_ts();
+
+    //清除sr
+    send_buf[0]=MMC5983MA_SR_ADDR;
+    send_buf[1]=2;
+    rt_spi_transfer(spi_dev,send_buf,recv_buf,2);
+
+    //跑一次init
+    send_buf[0]=MMC5983MA_CR1_ADDR;
+    send_buf[1]=gs_mmc5983ma_cr1.r;
+    rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
+
+    send_buf[0]=MMC5983MA_CR0_ADDR;
+    send_buf[1]=gs_mmc5983ma_cr0.r;
+    rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
+
+    send_buf[0]=MMC5983MA_CR2_ADDR;
+    send_buf[1]=gs_mmc5983ma_cr2.r;
+    rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
+//这个是可以的 但是采样率会降低
+    // send_buf[0]=MMC5983MA_CR2_ADDR;
+    // send_buf[1]=gs_mmc5983ma_cr2.r;
+    // rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
+    // send_buf[0]=MMC5983MA_CR0_ADDR;
+    // send_buf[1]=gs_mmc5983ma_cr0.r;
+    // rt_spi_transfer(spi_dev,send_buf,RT_NULL,2);
 
     return 1;
 }
